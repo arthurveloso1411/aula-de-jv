6c5c9e07a733976880ff09241ddda06bb60750a3
arthurveloso1411 <arthurvelosomacambira@gmail.com>
2026-06-18 13:35:47 -0300

    ultima aula

--- sistemadelogin.js
+++ sistemadelogin.js
@@ -1,12 +1,25 @@
-const prompt = require("prompt-sync")();
+ const prompt = require("prompt-sync")();
 const fs = require('fs');
 
 const dados = fs.readFileSync('users.json',"utf-8")
 const usuarios = JSON.parse(dados)
 
-const nome = prompt("digite seu nome")
-const idade = parseInt(prompt("digite sua idade"))
+console.log('Bem vindoo a programa de cadastro')
+const nome = prompt("Digite seu nome: ")
 
-const novoUsuario = {
-    
+console.log(`Voce digitou o nome ${nome}`)
+const idade = parseInt(prompt("digite sua idade: "))
+
+console.log(`Voce digitou a idade  ${idade}`)
+
+const novoUsuario= {
+    id: usuarios.length +1,
+    nome,
+    idade
 }
+
+usuarios.push(novoUsuario)
+
+fs.writeFileSync('users.json',JSON.stringify(usuarios,null,2));
+
+console.log(usuarios)
